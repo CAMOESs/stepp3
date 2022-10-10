48386afd34351d132911f22e4a8b9ca373dd28e8
camoes <cam@camoes.fr>
2022-10-10 19:52:41 +0100

    commit

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ApplicationRecord
     validates :deadline_on, presence: true
     validates :status, presence: true
     paginates_per 10
-    enum priority: { 高:0, 中:1, 低:2 }
+    enum priority: {高:0, 中:1, 低:2 }
     enum status: {未着手:0, 着手中:1, 完了:2}
 
     scope :statuS, ->(title,n) { where("title LIKE ? AND status = ?",title,n)}
