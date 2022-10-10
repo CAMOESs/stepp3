415762a50d6f7b417a855bbacfca24b4068fd51f
camoes <cam@camoes.fr>
2022-10-11 00:48:31 +0100

    commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,8 +6,7 @@ class TasksController < ApplicationController
       if params[:sort_deadline_on]
         @tasks = Task.deadline_oN.page params[:page]
       elsif params[:sort_priority] 
-        @tasks = Task.all.order(created_at: :desc).page params[:page]
-        @tasks.prioritY.page params[:page]
+        @tasks = Task.prioritY.page params[:page]
 
       elsif
         @sess = session[:search]
